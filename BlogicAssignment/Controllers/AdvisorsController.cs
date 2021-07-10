527496430f3d96268e285073fb43d2c3c73c89ed
Martin Zedníček <martin.zednicek@gmail.com>
2021-07-10 13:49:20 +0200

    Default Scaffolding - Advisors,Clients,Contracts controllers and views

---
+++ BlogicAssignment/Controllers/AdvisorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BlogicAssignment.Data;
+using BlogicAssignment.Models;
+
+namespace BlogicAssignment.Controllers
+{
+    public class AdvisorsController : Controller
+    {
+        private readonly BlogicAssignmentDbContext _context;
+
+        public AdvisorsController(BlogicAssignmentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Advisors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Advisors.ToListAsync());
+        }
+
+        // GET: Advisors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var advisor = await _context.Advisors
+                .FirstOrDefaultAsync(m => m.AdvisorID == id);
+            if (advisor == null)
+            {
+                return NotFound();
+            }
+
+            return View(advisor);
+        }
+
+        // GET: Advisors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Advisors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AdvisorID,FirstName,LastName,BirthNumber,Age,Phone,Email")] Advisor advisor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(advisor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(advisor);
+        }
+
+        // GET: Advisors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var advisor = await _context.Advisors.FindAsync(id);
+            if (advisor == null)
+            {
+                return NotFound();
+            }
+            return View(advisor);
+        }
+
+        // POST: Advisors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AdvisorID,FirstName,LastName,BirthNumber,Age,Phone,Email")] Advisor advisor)
+        {
+            if (id != advisor.AdvisorID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(advisor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdvisorExists(advisor.AdvisorID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(advisor);
+        }
+
+        // GET: Advisors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var advisor = await _context.Advisors
+                .FirstOrDefaultAsync(m => m.AdvisorID == id);
+            if (advisor == null)
+            {
+                return NotFound();
+            }
+
+            return View(advisor);
+        }
+
+        // POST: Advisors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var advisor = await _context.Advisors.FindAsync(id);
+            _context.Advisors.Remove(advisor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AdvisorExists(int id)
+        {
+            return _context.Advisors.Any(e => e.AdvisorID == id);
+        }
+    }
+}
---
+++ BlogicAssignment/Controllers/ContractsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BlogicAssignment.Data;
+using BlogicAssignment.Models;
+
+namespace BlogicAssignment.Controllers
+{
+    public class ContractsController : Controller
+    {
+        private readonly BlogicAssignmentDbContext _context;
+
+        public ContractsController(BlogicAssignmentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contracts
+        public async Task<IActionResult> Index()
+        {
+            var blogicAssignmentDbContext = _context.Contracts.Include(c => c.Client).Include(c => c.Supervisor);
+            return View(await blogicAssignmentDbContext.ToListAsync());
+        }
+
+        // GET: Contracts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contracts
+                .Include(c => c.Client)
+                .Include(c => c.Supervisor)
+                .FirstOrDefaultAsync(m => m.ContractID == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+
+        // GET: Contracts/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "BirthNumber");
+            ViewData["SupervisorID"] = new SelectList(_context.Advisors, "AdvisorID", "BirthNumber");
+            return View();
+        }
+
+        // POST: Contracts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContractID,EvidenceNumber,Institution,SupervisorID,ClientID,ContractEnterDate,ContractValidSinceDate,ContractEndDate")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contract);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "BirthNumber", contract.ClientID);
+            ViewData["SupervisorID"] = new SelectList(_context.Advisors, "AdvisorID", "BirthNumber", contract.SupervisorID);
+            return View(contract);
+        }
+
+        // GET: Contracts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "BirthNumber", contract.ClientID);
+            ViewData["SupervisorID"] = new SelectList(_context.Advisors, "AdvisorID", "BirthNumber", contract.SupervisorID);
+            return View(contract);
+        }
+
+        // POST: Contracts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContractID,EvidenceNumber,Institution,SupervisorID,ClientID,ContractEnterDate,ContractValidSinceDate,ContractEndDate")] Contract contract)
+        {
+            if (id != contract.ContractID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contract);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContractExists(contract.ContractID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientID"] = new SelectList(_context.Clients, "ClientID", "BirthNumber", contract.ClientID);
+            ViewData["SupervisorID"] = new SelectList(_context.Advisors, "AdvisorID", "BirthNumber", contract.SupervisorID);
+            return View(contract);
+        }
+
+        // GET: Contracts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contracts
+                .Include(c => c.Client)
+                .Include(c => c.Supervisor)
+                .FirstOrDefaultAsync(m => m.ContractID == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+
+        // POST: Contracts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contract = await _context.Contracts.FindAsync(id);
+            _context.Contracts.Remove(contract);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContractExists(int id)
+        {
+            return _context.Contracts.Any(e => e.ContractID == id);
+        }
+    }
+}
